2b25b3db3e0481805c717f3b0e6ad97c9e70b7dd
Анатолий <Chepanov199287@gmail.com>
2023-03-21 13:30:23 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillFactry_3_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double result = 5.0 / 2.0 * 3.0;
+        }
+    }
+}
